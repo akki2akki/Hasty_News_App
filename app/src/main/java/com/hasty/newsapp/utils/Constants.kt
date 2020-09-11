8c0bd929ee5dfcd2acbd8847de1b82900a342bb5
Ayush Mangla <ayushmangla00@gmail.com>
2020-09-11 13:17:05 +0530

    Update Constants.kt

--- app/src/main/java/com/hasty/newsapp/utils/Constants.kt
+++ app/src/main/java/com/hasty/newsapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.hasty.newsapp.utils
 
 object Constants {
 
-    const val NEWS_API_KEY = "ee869d5a767f4442a6021f5bc5a495dc"
+    const val NEWS_API_KEY = "Your_Api_Key"
     const val BASE_URL = "https://newsapi.org"
     const val QUERY_PAGE_SIZE = 20
     const val SEARCH_NEWS_TIME_DELAY = 500L
